ff15cacbe70e84e2b01dd64c17b919408b4a13b1
Mostafa Ebrahimi <mse.researcher@gmail.com>
2022-04-01 16:14:40 +0430

    install dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./container/Home";
+import Login from "./components/Login";
+function App() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
