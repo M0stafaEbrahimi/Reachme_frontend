624b3992e967857c1f48d9e9cb899223f88c067d
Mostafa Ebrahimi <mse.researcher@gmail.com>
2022-04-03 17:24:57 +0430

    add login page and implement logic for connect to database and create user

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import Home from "./container/Home";
-import Login from "./components/Login";
+import Home from "./container/Home.js";
+import Login from "./components/Login.js";
 function App() {
   return (
     <Routes>
