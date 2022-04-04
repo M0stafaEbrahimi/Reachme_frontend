3938890cbe5d1a57ee7f3eed031be45843961125
Mostafa Ebrahimi <mse.researcher@gmail.com>
2022-04-04 10:04:47 +0430

    add home and sidebar and userprofile component

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,5 +1,8 @@
-import React from "react";
-
+import React, { useEffect, useState, useRef } from "react";
+import { HiMenu } from "react-icons/hi";
+import { AiFillClockCircle } from "react-icons/ai";
+import { Link, Route, Routes } from "react-router-dom";
+import { Sidebar, UserProfile } from "../components";
 function Home() {
   return <div>Home</div>;
 }
